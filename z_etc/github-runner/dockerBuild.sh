3737461163f90936285a6be011afd457baa80a86
ulmsaga <sclee1115@gmail.com>
2025-03-26 11:02:43 +0900

    config test03

--- z_etc/github-runner/dockerBuild.sh
+++ z_etc/github-runner/dockerBuild.sh
@@ -14,4 +14,4 @@ docker buildx create --use --name mybuilder --driver docker-container --driver-o
 docker run --rm --privileged tonistiigi/binfmt --install all
 
 # 다중 아키텍처 이미지를 빌드하고 푸시
-docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64 -t sclee1115/github-runner-custom:v0.0.9 -f Dockerfile --push .
+docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64 -t sclee1115/github-runner-custom:v0.0.10 -f Dockerfile --push .
